23382166828ef10ddb7cc674339d2580c3e815e8
Abhishek Kumar Singh <abhishek.singh.789@outlook.com>
2020-07-05 13:15:58 +0530

    broken homepage in smaller component and styled it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import HomePage from './pages/homepage/homepage.component'
+
 import './App.css';
 
 class App extends Component {
@@ -6,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-  
+        <HomePage />
       </div>
     );
   }
